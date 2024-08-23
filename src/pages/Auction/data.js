100dc2cf4559aa55dcaadd34861193505287110b
PhunChiee <hoangphuongchi2003@gmail.com>
2024-08-23 22:00:24 +0700

    Chi

---
+++ src/pages/Auction/data.js
@@ -0,0 +1,70 @@
+import imgMenu from "../../assets/image/menu.png"
+
+export const listAuction = [
+    {
+        img: imgMenu,
+        title: "Long bào",
+        startprice: "2.000.000",
+        stepprice: "50.000",
+        startTime: "14:45 - 29/08/2025",
+        endTime: "17:45 - 29/08/2025"
+    },
+    {
+        img: imgMenu,
+        title: "Long bào",
+        startprice: "2.000.000",
+        stepprice: "50.000",
+        startTime: "14:45 - 29/08/2025",
+        endTime: "17:45 - 29/08/2025"
+    },
+    {
+        img: imgMenu,
+        title: "Long bào",
+        startprice: "2.000.000",
+        stepprice: "50.000",
+        startTime: "14:45 - 29/08/2025",
+        endTime: "17:45 - 29/08/2025"
+    },
+    {
+        img: imgMenu,
+        title: "Long bào",
+        startprice: "2.000.000",
+        stepprice: "50.000",
+        startTime: "14:45 - 29/08/2025",
+        endTime: "17:45 - 29/08/2025"
+    }
+]
+export const listAuction2 = [
+    {
+        img: imgMenu,
+        title: "Long bào",
+        startprice: "2.000.000",
+        stepprice: "50.000",
+        Solandaugia: "7",
+        endTime: true
+    },
+    {
+        img: imgMenu,
+        title: "Long bào",
+        startprice: "2.000.000",
+        stepprice: "50.000",
+        Solandaugia: "7",
+        endTime: true
+    },
+    {
+        img: imgMenu,
+        title: "Long bào",
+        startprice: "2.000.000",
+        stepprice: "50.000",
+        Solandaugia: "7",
+        endTime: true
+    },
+    {
+        img: imgMenu,
+        title: "Long bào",
+        startprice: "2.000.000",
+        stepprice: "50.000",
+        Solandaugia: "7",
+        endTime: true
+    }
+]
